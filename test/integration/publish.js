32431a453435d68fda5ca7187bc2799d480704cf
ehg <chris.blower@jumpwire.co.uk>
2013-07-23 15:53:34 +0100

    Publish integration test now allows environment variables to be set for testing different environments

--- test/integration/publish.js
+++ test/integration/publish.js
@@ -8,20 +8,25 @@ var wd = require('wd'),
 
 var browser = helper.browser,
   wd40 = helper.wd40;
-// TODO: allow specification of base URL, credentials and post id
+
+var BASE_URL = process.env.BASE_URL || 'http://localhost:8080/wordpress/',
+    WP_USER = process.env.WP_USER || 'admin',
+    WP_PASS = process.env.WP_PASS || 'vagrant',
+    WP_POST_ID = process.env.WP_POST_ID || '1';
+
 describe('Publishing a liveblog update', function() {
   context("When I'm logged in as an admin", function() {
     before( function( done ) {
       wd40.init( function(err) {
-        browser.get('http://localhost:8080/wordpress/wp-login.php', function(){
+        browser.get(BASE_URL + '/wp-login.php', function(){
           return done( err );
         });
       });
     });
 
     before( function( done ) {
-      wd40.fill( '#user_login', 'admin', function( err ){
-        wd40.fill( '#user_pass', 'vagrant', function( err ){
+      wd40.fill( '#user_login', WP_USER, function( err ){
+        wd40.fill( '#user_pass', WP_PASS, function( err ){
           wd40.click( '#wp-submit', function( err ){
             done( err );
           });
@@ -33,7 +38,7 @@ describe('Publishing a liveblog update', function() {
       var random = String(Math.random());
 
       before( function( done ) {
-        browser.get('http://localhost:8080/wordpress/?p=1', function( err ){
+        browser.get(BASE_URL + '/?p=1', function( err ){
           return done( err );
         });
       });
