6f2c2d67143bfec434638ae4eca4ffdd416b7226
ehg <chris.blower@jumpwire.co.uk>
2013-07-23 15:52:02 +0100

    Fix throttle args

--- js/liveblog.js
+++ js/liveblog.js
@@ -8,7 +8,7 @@ window.liveblog = window.liveblog || {};
 		el: '#liveblog-container',
 		initialize: function() {
 			liveblog.queue.on('reset', this.scrollToTop, this);
-			$(window).scroll(_.throttle(250, this.flushQueueWhenOnTop));
+			$(window).scroll(_.throttle(this.flushQueueWhenOnTop, 250));
 		},
 		scrollToTop: function() {
 			$(window).scrollTop(this.$el.offset().top);
